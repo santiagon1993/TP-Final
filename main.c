8c14748ca2d3c6570b33d125f96634eca6608369
FrancoEmmanuel95 <105375245+FrancoEmmanuel95@users.noreply.github.com>
2022-05-30 20:42:42 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,15 +3,21 @@
 #include <string.h>
 #include "header-tpfinal.h"
 #define DIM 1000
+#define clientes "clientes.dat"
+
 int main()
-{   stCliente stClientes[DIM];
-    int IDs=0;
+{   char archivoClientes[DIM];
+    stCliente stClientes[DIM];
+    int x=0;
+  //  int ID=0;
+   // char n[20],a[20];
 
-    stClientes[IDs]=altaCliente(IDs);
 
-
-
-
+    mostrarMenu();
+    seleccion();
+    /*stClientes[ID]=altaUnCliente(ID);
+    consultaCliente(stClientes[0]);
+    printf("\n\n%d",ID);*/
 
 
     return 0;
